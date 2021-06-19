6781a9afd7d11fddcb6bde3bcd3d34ee13a4f201
om <op999588@gmail.com>
2021-06-19 19:14:32 +0530

    first commit

---
+++ src/main/java/com/qa/util/ConfigReader.java
@@ -0,0 +1,26 @@
+package com.qa.util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+	private Properties prop;
+
+	/**
+	 * This method is used to load the properties from config.properties file
+	 * @return it returns Properties prop object
+	 * @throws IOException 
+	 */
+	public Properties init_prop() throws IOException {
+
+		prop = new Properties();
+	
+			FileInputStream ip = new FileInputStream("./src/test/resources/config/config.properties");
+			prop.load(ip);
+
+		return prop;
+
+	}
+}
